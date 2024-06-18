efaeb7da50519bd5e813c2ef1fd4e9ac4f12bf99
Aleks Ivanov <aleksivanov.me@gmail.com>
2024-06-18 23:06:07 +0200

    fix initdb to return connection pool + error

--- database.go
+++ database.go
@@ -9,26 +9,12 @@ import (
 
 var ctx = context.Background()
 
-var dbInstance *pgxpool.Pool
-
-func InitDB() error {
+func InitDB() (*pgxpool.Pool, error) {
 	pool, err := pgxpool.NewWithConfig(ctx, Config())
 	if err != nil {
 		log.Fatal("unable to connect to database: ", err)
-		return err
+		return pool, err
 	}
 
-	dbInstance = pool
-
-	return nil
-}
-
-func GetDB() *pgxpool.Pool {
-	return dbInstance
-}
-
-func CloseDB() {
-	if dbInstance != nil {
-		dbInstance.Close()
-	}
+	return pool, nil
 }
