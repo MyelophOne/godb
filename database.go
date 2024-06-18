e052c2fa512618a84d3df22948f8b249eb1ef8bb
Aleks Ivanov <aleksivanov.me@gmail.com>
2024-06-18 22:44:09 +0200

    fix context

--- database.go
+++ database.go
@@ -7,11 +7,9 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-var ctx = context.Background()
-
 var dbInstance *pgxpool.Pool
 
-func InitDB() error {
+func InitDB(ctx context.Context) error {
 	pool, err := pgxpool.NewWithConfig(ctx, Config())
 	if err != nil {
 		log.Fatal("unable to connect to database: ", err)
